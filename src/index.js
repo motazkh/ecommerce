6d8b09a8f90c075962bc6cc1416baee8fa70ad98
motazkh <motaz.khateeb@hotmail.com>
2024-10-01 11:03:32 +0300

    most sold product

--- src/index.js
+++ src/index.js
@@ -4,3 +4,9 @@ import "./css/style.css" ;
 import "@fortawesome/fontawesome-free/js/all.min" ; 
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+document.querySelectorAll(".add-to-cart-btn").forEach(item => {
+    item.addEventListener("click" , () => {
+        alert("أضيف المنتج الى عربة الشراء")
+    })
+})
